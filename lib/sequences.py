a3497af92b993206f47db304335b590a79761796
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-03-13 17:21:07 -0400

    Lab Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length == 0:
+       print(fibonacci)
+    elif length == 1:
+        fibonacci.append(0)
+        print(fibonacci)
+    elif length == 2:
+        fibonacci=[0,1]
+        print(fibonacci)
+    elif length > 2:
+        fibonacci = [0,1]
+        for i in range(2, length):
+            fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+        print(fibonacci)
+   
+print_fibonacci(6)
